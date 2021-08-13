fdd933a852efd208b56f88a740989baa306e0351
Jacob White <jacobwhite@WHITE-A1398.local>
2021-08-13 17:19:01 -0400

    Added routing, static assets, and the workout home page

--- App.js
+++ App.js
@@ -1,44 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Font from 'expo-font';
+import Splash from "./Splash.js"
+import HomeWorkouts from "./HomeWorkouts.js"
 
-export default function App() {
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Arnold<Text style={styles.period}>.</Text></Text>
-      <Text style={styles.subheader}>The simple gym tracker</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Splash"
+        onPress={() => navigation.navigate('Splash')}
+      />
+      <Button
+        title="Home Workouts"
+        onPress={() => navigation.navigate('HomeWorkouts')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: '-20%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    color: 'black',
-    fontSize: 72,
-    fontFamily: 'Josefin Sans', 
-    fontWeight: '800',
-  },
-  period: {
-    marginLeft: '-5%',
-    color: 'red',
-    fontSize: 150,
-    fontFamily: 'Josefin Sans', 
-    fontWeight: 'bold',
-  },
-  subheader: {
-    marginTop: '-5%',
-    color: 'black',
-    fontSize: 30,
-    fontFamily: 'Ludo',
-    fontWeight: '200'
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default class App extends React.Component {
+  async componentDidMount() {
+    await Font.loadAsync({
+      'JosefinSans-Regular': require('./assets/fonts/JosefinSans-Regular.ttf'),
+      'JosefinSans-SemiBold': require('./assets/fonts/JosefinSans-SemiBold.ttf'),
+
+      'JosefinSans-Bold': require('./assets/fonts/JosefinSans-Bold.ttf'),
+      'Lato-Light': require('./assets/fonts/Lato-Light.ttf'),
+    });
   }
 
-});
+  render() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Home Screen" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="HomeWorkouts" component={HomeWorkouts} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  }
+}
+
