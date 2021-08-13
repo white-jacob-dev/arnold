f9a6714c3fde4ec5c822335b28194561ee7d3cb9
Jacob White <white.jacob.dev@gmail.com>
2021-08-13 00:49:52 -0400

    Created splash screen

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Arnold<Text style={styles.period}>.</Text></Text>
+      <Text style={styles.subheader}>The simple gym tracker</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: '-20%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    color: 'black',
+    fontSize: 72,
+    fontFamily: 'Josefin Sans', 
+    fontWeight: '800',
+  },
+  period: {
+    marginLeft: '-5%',
+    color: 'red',
+    fontSize: 150,
+    fontFamily: 'Josefin Sans', 
+    fontWeight: 'bold',
+  },
+  subheader: {
+    marginTop: '-5%',
+    color: 'black',
+    fontSize: 30,
+    fontFamily: 'Ludo',
+    fontWeight: '200'
+  }
+
 });
